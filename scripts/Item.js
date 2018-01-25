c82a9edb00eed9c0b1ae15f99b910f7d4e87ccc1
George Luke <george.luke92@gmail.com>
2018-01-25 17:17:45 -0600

    update HTML, work on CSS, copy previous behaviors implemented in shopping list app

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,1 +1,25 @@
-console.log('hi, Item.js here');
+'use strict';
+
+const Item = (function () {
+
+  const validateName = function (name) {
+    if (!name) throw new TypeError('Name must not be blank');
+  };
+
+  const create = function (name) {
+    return {
+      id: cuid(),
+      name,
+      rating: false,
+      newTitle: /* from event listener */
+      editing: false /* triggered by event listener on title */
+    };
+  };
+
+  return {
+    validateName,
+    create,
+  };
+
+}());
+
