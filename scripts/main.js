bc88fdc32d5b34ee1d23b4683e5f8f9580ca9560
George Luke <george.luke92@gmail.com>
2018-01-26 11:20:06 -0600

    updated main.js with shopping-list material: need to update with correct variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log('this is main.js');
+$(document).ready(function () {
+    shoppingList.bindEventListeners();
+    shoppingList.render();
+    api.getItems((items) => {
+        items.forEach((item) => store.addItem(item));
+        /* test code */
+        // const item = store.items[0];
+        // console.log('current name: ' + item.name);
+        // store.findAndUpdate(item.id, { name: 'stuff' });
+        // console.log('new name: ' + item.name);
+        shoppingList.render();
+    });
+});
